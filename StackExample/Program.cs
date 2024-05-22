c913ee8376f31615d4daa9822476e0c7a0d5b259
Koninti Mohithkumar Goud <al6127@accionlabs.in>
2024-05-22 17:28:45 +0530

    first commit

---
+++ StackExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample
+{
+   class Program
+    {
+        static void Main()
+        {
+            Stack<int> stack = new Stack<int>();    
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Pop();
+           foreach(int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+           System.Console.WriteLine("Lat Element in Stack : "+stack.Peek());
+            Console.WriteLine("Number of elements in stack : "+stack.Count());
+            Console.ReadKey();
+        }
+    }
+}
